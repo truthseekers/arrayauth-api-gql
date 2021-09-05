88027eab0a9a34e95348501ac51377fbe08203fc
John Curry <fastpenguin91@gmail.com>
2021-09-05 13:03:36 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,52 @@
+![TruthSeekers Logo](https://truthseekers.io/wp-content/uploads/2021/05/ts-logo-dark-horizontal549x181.jpg)
+
+## [Please support our YouTube channel by Subscribing!](https://www.youtube.com/channel/UCa0s8d-23qP7RmIMZ54x7Ug)
+
+### Develop your skills with our quality tutorials on:
+
+Linux, Docker & Kubernetes, React.js, GraphQL, SQL, Mongo, Emacs & more. Literally anything tech.
+
+## [Join our community by signing up for our newsletter!](https://truthseekers.io/latest-tutorials-signup/)
+
+
+# Lectures & Branches * Add a start script to package.json for any branch you checkout! *
+
+Lecture: GraphQL Hello Worl with graphql-yoga
+Branch - hello-world-done
+
+Lecture: Querying an array of Users
+Branch - querying-data-done
+
+Lecture: Field Level Resolvers in GraphQL
+Branch - field-level-resolvers-done
+
+Lecture: Challenge: add age with multiplier
+Branch - change-age-done && change-age-start
+
+Lecture: Filtering results with parameters
+Branch - challenge-query-parameters-done (and -start)
+
+Lecture: Mutations: Creating a user
+Branch - mutation-create-user-done (and -start)
+
+Lecture: Auto Increment User Id
+Branch - auto-increment-id-challenge-done (and -start)
+
+Lecture: Delete a User
+Branch - challenge-delete-user-done (and -start)
+
+Lecture: Challenge: update User
+Branch - challenge-update-user-done (and -start)
+
+Lecture: Input Types in GraphQL
+Branch - input-types-done (and -start)
+
+Lecture: One to many relationships
+Branch - one-to-many-done (and -start)
+
+Lecture: add and delete todos
+Branch - add-delete-todos-done (and -start)
+
+Lecture: Many to Many relationships
+Branch - many-to-many-done (and -start)
+
---
+++ src/resolvers.js
@@ -0,0 +1,118 @@
+const { createUserId } = require("./utils");
+
+const newUserId = createUserId();
+const newTodoId = createUserId(); // only temporary.
+
+const { users } = require("./data/users");
+const { todos } = require("./data/todos");
+
+const resolvers = {
+  Query: {
+    helloWorld: () => `Hello world! what a day!`,
+    users: (parent, args, context, info) => {
+      if (args.text) {
+        return users.filter((elem) =>
+          elem.firstName.toLowerCase().includes(args.text.toLowerCase())
+        );
+      }
+      return users;
+    },
+    user: (parent, args, context, info) => {
+      console.log(args);
+      return users.find((user) => {
+        if (user.id == args.userId) {
+          return user;
+        }
+      });
+    },
+    todos: (parent, args, context, info) => {
+      return todos;
+    },
+  },
+  Mutation: {
+    createUser: (parent, args, context, info) => {
+      const userAlreadyExists = users.some((elem) => {
+        return elem.email == args.email;
+      });
+
+      if (userAlreadyExists) {
+        throw new Error("User already exists");
+      } else {
+        const newUser = {
+          id: newUserId(),
+          firstName: args.firstName,
+          email: args.email,
+          age: args.age,
+        };
+        users.push(newUser);
+        return newUser;
+      }
+    },
+    deleteUser: (parent, args, context, info) => {
+      let user;
+      const userToRemove = users.findIndex((elem) => {
+        if (elem.id == args.userId) {
+          user = elem;
+          return true;
+        }
+        return false; // passes test so stays in array.
+      });
+      user ? users.splice(userToRemove, 1) : "";
+      return user;
+      // ? `Account for ${user.email} was deleted`
+      // : "No user to delete";
+    },
+    updateUser: (_, args) => {
+      const userIndex = users.findIndex((elem) => elem.id == args.userId);
+
+      users[userIndex] = { ...users[userIndex], ...args.input };
+      return users[userIndex];
+    },
+    createTodo: (parent, args, context, info) => {
+      const newTodo = {
+        id: newTodoId(),
+        name: args.name,
+        isComplete: args.isComplete,
+        userId: args.userId,
+      };
+
+      todos.push(newTodo);
+      return newTodo;
+    },
+    deleteTodo: (parent, args, context, info) => {
+      let todo;
+      const todoToRemove = todos.findIndex((elem) => {
+        if (elem.id == args.todoId) {
+          todo = elem;
+          return true;
+        }
+        return false;
+      });
+      todo ? todos.splice(todoToRemove, 1) : "";
+      return todo;
+    },
+  },
+  User: {
+    id: (parent) => parent.id,
+    firstName: (parent) => {
+      return parent.firstName;
+    },
+    email: (parent) => parent.email,
+    age: (parent) => {
+      return parent.age;
+    },
+    todos: (parent) => {
+      console.log("parent: ", parent);
+      return todos.filter((elem) => elem.userId == parent.id);
+    },
+  },
+  Todo: {
+    user: (parent) => {
+      return users.find((elem) => elem.id == parent.userId);
+    },
+  },
+};
+
+module.exports = {
+  resolvers,
+};
